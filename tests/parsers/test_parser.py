e1eff34cf2812e4e71869c8df5e898949b498d6c
Guillaume Gaisne <guillaume.gaisne@esrf.fr>
2025-07-16 16:19:10 +0200

    Add test files

--- tests/parsers/test_parser.py
+++ tests/parsers/test_parser.py
@@ -8,6 +8,6 @@ from nomad_dfxm_parser.parsers.parser import NewParser
 def test_parse_file():
     parser = NewParser()
     archive = EntryArchive()
-    parser.parse('tests/data/example.out', archive, logging.getLogger())
+    parser.parse('tests/data/example_DFXM_file.h5', archive, logging.getLogger())
 
-    assert archive.workflow2.name == 'test'
+    assert archive.data.definition == 'DFXM'
